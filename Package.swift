da9c4b66af692c4a5680732f71782986f2a8af3d
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-10-10 21:56:05 +1000

    Added GUUnits dependency.

--- Package.swift
+++ Package.swift
@@ -14,16 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "ssh://git.mipal.net/git/swift_wb", .branch("master"))
+        .package(url: "ssh://git.mipal.net/git/swift_wb", .branch("master")),
+        .package(url: "ssh://git.mipal.net/git/swift_GUUnits.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftfsmWBWrappers",
-            dependencies: ["GUSimpleWhiteboard"]),
+            dependencies: ["GUSimpleWhiteboard", "GUUnits"]),
         .testTarget(
             name: "SwiftfsmWBWrappersTests",
-            dependencies: ["SwiftfsmWBWrappers"]),
+            dependencies: ["SwiftfsmWBWrappers", "GUUnits"]),
     ]
 )
