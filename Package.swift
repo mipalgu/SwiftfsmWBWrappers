94008e6ecff44d368460fbea48ffbfeeb4707b54
Callum McColl <callum.mccoll1@gmail.com>
2020-10-19 03:42:43 +1000

    Depend on the master branch of FSM

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "ssh://git.mipal.net/git/swift_wb", .branch("master")),
         .package(url: "ssh://git.mipal.net/git/swift_GUUnits.git", .branch("master")),
         .package(url: "ssh://git.mipal.net/git/swift_GUCoordinates.git", .branch("master")),
-        .package(url: "ssh://git.mipal.net/git/swiftfsm_FSM.git", .branch("binaries"))
+        .package(url: "ssh://git.mipal.net/git/swiftfsm_FSM.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
