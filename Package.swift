e6e18a7c8acd76d9e2bae8ccfb7cec89dcb02763
Callum McColl <callum.mccoll1@gmail.com>
2021-02-05 08:06:58 +1000

    Updated Package.swift to use the new /Users/Shared/git url.

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "ssh://git.mipal.net/git/swift_wb", .branch("master")),
-        .package(url: "ssh://git.mipal.net/git/swift_GUUnits.git", .branch("master")),
-        .package(url: "ssh://git.mipal.net/git/swift_GUCoordinates.git", .branch("master"))
+        .package(url: "ssh://git.mipal.net/Users/Shared/git/swift_wb", .branch("master")),
+        .package(url: "ssh://git.mipal.net/Users/Shared/git/swift_GUUnits.git", .branch("master")),
+        .package(url: "ssh://git.mipal.net/Users/Shared/git/swift_GUCoordinates.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
